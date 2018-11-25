a7a1ded0dab5cbf4651fa283795fc058372e2871
Oleksandr Rudyi <calllok.rud@gmail.com>
2018-11-25 20:59:32 +0200

    Initial commit

---
+++ PictureEditor/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace PictureEditor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.redBar = new System.Windows.Forms.TrackBar();
+            this.greenBar = new System.Windows.Forms.TrackBar();
+            this.blueBar = new System.Windows.Forms.TrackBar();
+            this.OpenButton = new System.Windows.Forms.Button();
+            this.SaveButton = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.RestoreButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.redBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.greenBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.blueBar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(776, 294);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 329);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "RED";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 361);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "GREEN";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 393);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "BLUE";
+            // 
+            // redBar
+            // 
+            this.redBar.LargeChange = 1;
+            this.redBar.Location = new System.Drawing.Point(53, 329);
+            this.redBar.Maximum = 20;
+            this.redBar.Name = "redBar";
+            this.redBar.Size = new System.Drawing.Size(608, 45);
+            this.redBar.TabIndex = 4;
+            this.redBar.ValueChanged += new System.EventHandler(this.redBar_ValueChanged);
+            // 
+            // greenBar
+            // 
+            this.greenBar.LargeChange = 1;
+            this.greenBar.Location = new System.Drawing.Point(53, 361);
+            this.greenBar.Maximum = 20;
+            this.greenBar.Name = "greenBar";
+            this.greenBar.Size = new System.Drawing.Size(608, 45);
+            this.greenBar.TabIndex = 5;
+            this.greenBar.ValueChanged += new System.EventHandler(this.greenBar_ValueChanged);
+            // 
+            // blueBar
+            // 
+            this.blueBar.LargeChange = 1;
+            this.blueBar.Location = new System.Drawing.Point(53, 393);
+            this.blueBar.Maximum = 20;
+            this.blueBar.Name = "blueBar";
+            this.blueBar.Size = new System.Drawing.Size(608, 45);
+            this.blueBar.TabIndex = 6;
+            this.blueBar.ValueChanged += new System.EventHandler(this.blueBar_ValueChanged);
+            // 
+            // OpenButton
+            // 
+            this.OpenButton.Location = new System.Drawing.Point(667, 329);
+            this.OpenButton.Name = "OpenButton";
+            this.OpenButton.Size = new System.Drawing.Size(121, 23);
+            this.OpenButton.TabIndex = 7;
+            this.OpenButton.Text = "Open";
+            this.OpenButton.UseVisualStyleBackColor = true;
+            this.OpenButton.Click += new System.EventHandler(this.OpenButton_Click);
+            // 
+            // SaveButton
+            // 
+            this.SaveButton.Location = new System.Drawing.Point(667, 361);
+            this.SaveButton.Name = "SaveButton";
+            this.SaveButton.Size = new System.Drawing.Size(121, 23);
+            this.SaveButton.TabIndex = 8;
+            this.SaveButton.Text = "Save";
+            this.SaveButton.UseVisualStyleBackColor = true;
+            this.SaveButton.Click += new System.EventHandler(this.SaveButton_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // RestoreButton
+            // 
+            this.RestoreButton.Location = new System.Drawing.Point(667, 393);
+            this.RestoreButton.Name = "RestoreButton";
+            this.RestoreButton.Size = new System.Drawing.Size(121, 23);
+            this.RestoreButton.TabIndex = 9;
+            this.RestoreButton.Text = "Restore";
+            this.RestoreButton.UseVisualStyleBackColor = true;
+            this.RestoreButton.Click += new System.EventHandler(this.RestoreButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.RestoreButton);
+            this.Controls.Add(this.SaveButton);
+            this.Controls.Add(this.OpenButton);
+            this.Controls.Add(this.blueBar);
+            this.Controls.Add(this.greenBar);
+            this.Controls.Add(this.redBar);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Picture Editor";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.redBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.greenBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.blueBar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TrackBar redBar;
+        private System.Windows.Forms.TrackBar greenBar;
+        private System.Windows.Forms.TrackBar blueBar;
+        private System.Windows.Forms.Button OpenButton;
+        private System.Windows.Forms.Button SaveButton;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button RestoreButton;
+    }
+}
+
---
+++ PictureEditor/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+using System.IO;
+
+namespace PictureEditor
+{
+    public partial class Form1 : Form
+    {
+        private Image imageFile;
+        private bool isOpened = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        public void OpenImage()
+        {
+            DialogResult dialogResult = openFileDialog1.ShowDialog();
+            if(dialogResult == DialogResult.OK)
+            {
+                imageFile = Image.FromFile(openFileDialog1.FileName);
+                pictureBox1.Image = imageFile;
+                isOpened = true;
+            }
+        }
+
+        public void SaveImage()
+        {
+            if(isOpened)
+            {
+                SaveFileDialog saveFileDialog = new SaveFileDialog();
+                saveFileDialog.Filter = "Images|*.png,*.bmp,*.jpg";
+                // Default format
+                ImageFormat imageFormat = ImageFormat.Png;
+                if(saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string extention = Path.GetExtension(saveFileDialog.FileName);
+                    switch(extention)
+                    {
+                        case ".jpg":
+                            imageFormat = ImageFormat.Jpeg;
+                            break;
+                        case ".bmp":
+                            imageFormat = ImageFormat.Bmp;
+                            break;
+                    }
+                    pictureBox1.Image.Save(saveFileDialog.FileName, imageFormat);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Nothing to save!");
+            }
+        }       
+
+        public void Reload()
+        {
+            if(!isOpened)
+            {
+                MessageBox.Show("Open the image than apply changes.");
+            }
+            else
+            {
+                if(isOpened)
+                {
+                    imageFile = Image.FromFile(openFileDialog1.FileName);
+                    pictureBox1.Image = imageFile;
+                    isOpened = true;
+                }
+
+            }
+        }
+
+        public void UseRGBTrackBars()
+        {
+            float changered = redBar.Value * 0.1f;
+            float changegreen = greenBar.Value * 0.1f;
+            float changeblue = blueBar.Value * 0.1f;
+
+            Reload();
+            if (isOpened)
+            {
+                Image image = pictureBox1.Image;
+                Bitmap bmpInverted = new Bitmap(image.Width, image.Height);
+                ImageAttributes imageAttributes = new ImageAttributes();
+                ColorMatrix colorMatrixPicture = new ColorMatrix(new float[][]
+                {
+                    new float[]{1+changered, 0, 0, 0, 0},
+                    new float[]{0, 1+changegreen, 0, 0, 0},
+                    new float[]{0, 0, 1+changeblue, 0, 0},
+                    new float[]{0, 0, 0, 1, 0},
+                    new float[]{0, 0, 0, 0, 1}
+                });
+                imageAttributes.SetColorMatrix(colorMatrixPicture);
+                Graphics g = Graphics.FromImage(bmpInverted);
+                g.DrawImage(image, new Rectangle(0, 0, image.Width, image.Height), 
+                    0, 0, image.Width, image.Height, GraphicsUnit.Pixel, imageAttributes);
+                g.Dispose();
+                pictureBox1.Image = bmpInverted;
+            }
+        }
+
+        private void OpenButton_Click(object sender, EventArgs e)
+        {
+            OpenImage();
+        }
+
+        private void SaveButton_Click(object sender, EventArgs e)
+        {
+            SaveImage();
+        }
+
+        private void redBar_ValueChanged(object sender, EventArgs e)
+        {
+            UseRGBTrackBars();
+        }
+
+        private void greenBar_ValueChanged(object sender, EventArgs e)
+        {
+            UseRGBTrackBars();
+        }
+
+        private void blueBar_ValueChanged(object sender, EventArgs e)
+        {
+            UseRGBTrackBars();
+        }
+
+        private void RestoreButton_Click(object sender, EventArgs e)
+        {
+            Reload();
+            redBar.Value = 0;
+            greenBar.Value = 0;
+            blueBar.Value = 0;
+        }
+    }
+}
